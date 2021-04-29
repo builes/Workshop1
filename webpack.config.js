36383aa1aff97651a134675e93578df25ca91a76
Mauricio Builes <rmbuilesm@unal.edu.co>
2021-04-29 09:12:42 -0500

    paquetes configurados correctamente

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/app/index.js",
+  mode: "production", //Aca ponemos el modo(development, production), por defecto viene en production
+
+  //asi lo ponemos para que cuando note cambios se actualice el servidor npx webpack serve
+  watch: true,
+
+  //Asi configuramos el puerto donde va a correr el servidor, por defecto viene en el 8080
+  devServer: {
+    port: 9000,
+    open: true,
+  },
+
+  //Ruta donde creara los archivos
+  output: { filename: "app.bundle.js", path: path.resolve(__dirname, "build") },
+
+  plugins: [
+    //Esto lo que hace es convertir el html de la ruta que le digamos en un nuevo archivo html en la carpeta bunlde
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+      },
+    }),
+  ],
+};
